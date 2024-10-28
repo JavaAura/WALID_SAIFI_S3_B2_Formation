e891b811e8e7f8070e47db9e0c0bbd9147d01691
Walid Saifi <129198510+WALIDSAIFI@users.noreply.github.com>
2024-10-28 14:56:41 +0100

    Add files via upload

---
+++ Gestion_Formation/src/main/java/com/Formation/Gestion/GestionFormationApplication.java
@@ -0,0 +1,13 @@
+package com.Formation.Gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFormationApplication.class, args);
+	}
+
+}
---
+++ Gestion_Formation/src/test/java/com/Formation/Gestion/GestionFormationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Formation.Gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFormationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0ed4b0da9ef8408e2cec55c51c363ac1dc95238b
Walid Saifi <saifiwalid21@gmail.com>
2024-10-28 16:10:03 +0100

    add.

--- Gestion_Formation/src/main/java/com/Formation/Gestion/Controller/test.java
+++ Gestion_Formation/src/main/java/com/Formation/Gestion/Controller/test.java
@@ -8,6 +8,6 @@ public class test{
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, YouCode!";
+        return "Hello,---------------------------------YouCode!";
     }
 }
